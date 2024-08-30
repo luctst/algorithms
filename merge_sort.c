085bf986dd1b31f4ea01c8bb8a60a4105a832645
luctst <lucas.tostee@gmail.com>
2024-08-30 13:13:34 +0200

    feat: add merge sort algo

---
+++ merge_sort.c
@@ -0,0 +1,93 @@
+/*
+ * Merge sort algorithm
+ * Goal: Sort Array
+ * Approach:
+ *  1. Divide the n-elements sequence to be sorted into two subsequences of n/2 elements each.
+ *  2. Sort the two sorted subsequences recursively using merge sort until two sequences of length n/2 are merged.
+ *  3. Merge the two sorted subsequences to produce the sorted answer
+*/
+
+#include <stdio.h>
+
+// Function to merge two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r) { 
+		printf("%d %d %d ", l, m, r);
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+
+// Function to print an array
+void printArray(int A[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+// l is for left index and r is right index of the sub-array of arr to be sorted
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for large l and r
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        // Merge the sorted halves
+				merge(arr, l, m, r);
+    }
+}
+
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7, 1};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
+}
